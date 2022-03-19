e121b54e28b2f825f9b0c6a8c9cf6e327c927c1a
z3rokwq <99331354+z3rokwq@users.noreply.github.com>
2022-03-19 18:29:33 +0300

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -59,3 +59,16 @@ $('#type').typed({
     typeSpeed: 40,
     loop: true,
 })
+
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: false,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 3,
+    },
+});
